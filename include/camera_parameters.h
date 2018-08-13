b6cb378859c6547b20c61921f08d09bddc56ed07
Andreas Lydakis <andreas.lydakis@innotecuk.com>
2018-08-13 08:50:39 +0100

    working commit

--- include/camera_parameters.h
+++ include/camera_parameters.h
@@ -1,12 +1,19 @@
 //
-// Created by andreas on 8/7/18.
+// Created by Andreas Lydakis on 8/7/18.
 //
 
 #ifndef VISUAL_ODOM_CAMERA_PARAMETERS_H
 #define VISUAL_ODOM_CAMERA_PARAMETERS_H
 
+/** @name Default Camera Parameters
+*  Safe default values for camera parameters.
+*/
+/**@{*/
+///Default Camera Width
 #define WIDTH 640
+///Default Camera Height
 #define HEIGHT 480
+/**@}*/
 
 #include <vector>
 #include "math.h"
@@ -14,93 +21,198 @@
 #include <cv_bridge/cv_bridge.h>
 #include "opencv2/calib3d/calib3d.hpp"
 
+/**
+ * A container class for camera parameters, read from a ROS camera calibration file, loaded
+ * with the ROS parameter server
+ */
 class CameraParameters {
 public:
-    CameraParameters(ros::NodeHandle nh, std::string calibration_file);
-
+    /**
+     * Constructor
+     * @param nh the ros::Nodehandle that handles reading from the ROS parameter server
+     */
     CameraParameters(ros::NodeHandle nh);
 
+    /**
+     * Convert from radians to degrees
+     * @param radians An angle in radians
+     * @return The value in degrees
+     */
     float radiansToDegrees(float radians);
 
+    /**
+     * Convert from degrees to radians
+     * @param degrees An angle in degrees
+     * @return the value in radians
+     */
     float degreesToRadians(float degrees);
 
+    /**
+     * Normalize an angle in degrees to [- \ref M_PI, \ref M_PI]
+     * @param degrees An angle in degrees
+     * @return The normalized angle
+     */
     float normalizeDegrees(float degrees);
 
+    /**
+     * Set the tild of the camera in degrees
+     * @param degrees The tilt of the camera in degrees (CameraParameters::tilt_degrees_)
+     */
     void setTiltDegrees(float degrees);
 
-    void setTiltRadians(float degrees);
+    /**
+     * Set the tilt of the camera in radians
+     * @param radians The tilt of the camera in radians (CameraParameters::tilt_degrees_)
+     */
+    void setTiltRadians(float radians);
 
+    /**
+     * Set the distance of the camera from the ground
+     * @param height The height of the camera (meters) (CameraParameters::distance_from_ground_)
+     */
     void setDistanceFromGround(float height);
 
+    /**
+     * Set the distance of the camera from the center of the platform
+     * @param distance Distance from center (meters) (CameraParameters::distance_from_center_)
+     */
     void setDistanceFromCenter(float distance);
 
+    /**
+     * Set image height
+     * @param height The new image height(pixels) (CameraParameters::height_)
+     */
     void setHeight(float height);
 
+    /**
+     * Set image width
+     * @param width The new image width(pixels) (CameraParameters::width_)
+     */
     void setWidth(float width);
 
+    /**
+     * Set the path of the camera calibration file
+     * @todo Not yet implemented (the parameters are read directly from the ROS parameter server)
+     * @param calibration_file the path to the camera calibration file
+     */
     void setCalibrationFile(std::string calibration_file);
 
-    void setImageTopic(std::string image_topic);
-
-    void setTwistTopic(std::string twist_topic);
-
-    void setOdomTopic(std::string odom_topic);
-
+    /**
+     * Set the horizontal subtended meters for the camera
+     * @todo Not yet implemented or used
+     * @param meters the horizontal subtended meters for the camera
+     */
     void setSubtensionHorizontal(float meters);
 
+    /**
+     * Set the vertical subtended meters for the camera
+     * @todo Not yet implemented or used
+     * @param meters the vertical subtended meters for the camera
+     */
     void setSubtensionVertical(float meters);
 
+    /**
+     * Return the distance from the ground for the camer
+     * @return The distance from ground (CameraParameters::distance_from_ground_)
+     */
     float getDistanceFromGround();
 
+    /**
+     * Return the distance from the ground for the camer
+     * @return The distance from ground (CameraParameters::distance_from_center_)
+     */
     float getDistanceFromCenter();
 
+    /**
+     * Return the tilt of the camera
+     * @return The tilt of the camera (CameraParameters::tilt_degrees_)
+     */
     float getTilt();
 
+    /**
+     * Return the image height
+     * @return The image height (CameraParameters::height_)
+     */
     int getHeight();
 
+    /**
+     * Return the image width
+     * @return The image width (CameraParameters::width_)
+     */
     int getWidth();
 
+    /**
+     * Return the horizontal subtended meters
+     * @todo not used currently
+     * @return The horizontal subtended meters (CameraParameters::subtension_horizontal_m_)
+     */
     float getSubtensionHorizontal();
 
+    /**
+     * Return the vertical subtended meters
+     * @todo not used currently
+     * @return The vertical subtended meters (CameraParameters::subtension_vertical_m_)
+     */
     float getSubtensionVertical();
 
+    /**
+     * Return the path to the camera calibration file
+     * @return The path to the camera calibration file (CameraParameters::calibration_file_)
+     */
     std::string getCalibrationFile();
 
-    std::string getImageTopic();
-
-    std::string getTwistTopic();
-
-    std::string getOdomTopic();
-
+    /**
+     * Return the camera calibration matrix
+     * @return The camera calibration matrix (CameraParameters::calibration_matrix_)
+     */
     cv::Matx33d &getCameraCalibrationMatrix();
 
-    cv::Matx33d &getWMatrix();
-
+    /**
+     * Return the camera distortion coefficients
+     * @return The camera distortion coefficients matrix (CameraParameters::distortion_coefficients_)
+     */
     cv::Mat &getDistortionCoefficients();
 
+    /**
+     * Return the camera rectification matrix
+     * @return The camera rectification matrix (CameraParameters::rectification_matrix_)
+     */
     cv::Mat &getRectificationMatrix();
 
+    /**
+     * Return the camera projection matrix
+     * @return The camera projection matrix (CameraParameters::projection_matrix_)
+     */
     cv::Mat &getProjectionMatrix();
 
 
 protected:
 private:
+    ///Handles the reading from the parameter server
     ros::NodeHandle nodeHandle_;
+    ///Path to file that contains the camera calibration results
     std::string calibration_file_;
-    std::string image_topic_;
-    std::string odom_topic_;
-    std::string twist_topic_;
+    ///Image height(pixels)
     int height_;
+    ///Image width(pixels)
     int width_;
+    ///Camera distance from the center of the robot (meters)
     float distance_from_center_;
+    ///Camera distance from the ground (meters)
     float distance_from_ground_;
+    ///Camera tilt(degrees)
     float tilt_degrees_;
+    ///Camera vertical subtendension (meters)
     float subtension_vertical_m_;
+    ///Camera horizontal subtendension (meters)
     float subtension_horizontal_m_;
+    ///3x3 camera calibration matrix
     cv::Matx33d camera_calibration_matrix_;
-    cv::Matx33d W_;
+    ///Camera distortion coefficients
     cv::Mat distortion_coefficients_;
+    ///Camera rectification matrix
     cv::Mat rectification_matrix_;
+    ///Camera projection matrix
     cv::Mat projection_matrix_;
 };
 
--- include/optical_flow.h
+++ include/optical_flow.h
@@ -1,16 +1,32 @@
 //
-// Created by andreas on 8/7/18.
+// Created by Andreas Lydakis on 8/7/18.
 //
 
 #ifndef VISUAL_ODOM_OPTICAL_FLOW_H
 #define VISUAL_ODOM_OPTICAL_FLOW_H
 
+/** @defgroup defines_
+ *  @name Default Constructor Parameters
+ *  Safe default values for constructor parameters.
+ */
+/**@{*/
+///Number of motion measurements to average over
 #define FILTER_SIZE 5
+///Create a new set of tracked features if more than this have been dropped between 2 iterations
 #define DROPPED_THRESHOLD 0.75
+///Number of points to keep track of
 #define MAX_POINTS 300
+///Ignore movement smaller than this
 #define DEADBAND_M 0.0005
-#define ROBOT_RADIUS 0.15
+///Radius of robot base
+#define ROBOT_RADIUS 0.44
+///Constant for motion estimate, experiment with the robot and find the best value for your case
 #define ROTATION_CONSTANT 1.57
+///Default topic to listen for images
+#define DEFAULT_IMAGE_TOPIC "/usb_cam/image_raw"
+///Default topic to publish motion estimation
+#define DEFAULT_TWIST_TOPIC "/twist/visual"
+/**@}*/
 
 #include <iostream>
 #include <vector>
@@ -21,7 +37,6 @@
 #include <image_transport/image_transport.h>
 
 #include "sensor_msgs/image_encodings.h"
-#include "geometry_msgs/Pose2D.h"
 #include "geometry_msgs/Twist.h"
 #include "compressed_image_transport/compressed_subscriber.h"
 #include "compressed_image_transport/compression_common.h"
@@ -33,95 +48,221 @@
 
 #include "camera_parameters.h"
 
+/**
+ * Class that provides a motion estimate between consecutive images. Uses images streamed in a ros topic
+ * and publishes a geometry_msgs/Twist message. To be used with other odometry sources such as IMUs.
+ * Heavily based on  <a href="https://github.com/apollack11/advanced-computer-vision">https://github.com/apollack11/advanced-computer-vision</a>
+ */
 class OpticalFlow {
 public:
-    explicit OpticalFlow(ros::NodeHandle nh, std::string image_topic = "/usb_cam/image_raw",
-                         std::string twist_topic = "/twist/visual",
-                         int filter_size = FILTER_SIZE, double dropped_threshold = DROPPED_THRESHOLD,
-                         int max_points = MAX_POINTS, double deadband = DEADBAND_M, double robot_radius = ROBOT_RADIUS,
+    /**
+     * Constructor: The only required argument is a ros::NodeHandle, the rest can assume default values
+     * but should be set depending on your implementation
+     *
+     * @param nh Handles all the ROS communications
+     * @param image_topic ROS topic where the camera images are being published. See OpticalFlow::image_subscriber_
+     * @param twist_topic ROS topic where the calculated motion is published. See OpticalFlow::twist_publisher_
+     * @param dropped_threshold Calculate new features if more than this have been lost between frames
+     * @param filter_size The number of motion estimates that is used to calculate the average motion
+     * @param max_points The number of features to track
+     * @param deadband Ignore motion smaller than this (meters)
+     * @param robot_radius The radius of the robot base (meters)
+     * @param rotation_constant Constant that is used for the motion calculation, it should be experimentally determined for
+     * each platform
+     */
+    explicit OpticalFlow(ros::NodeHandle nh,
+                         std::string image_topic = DEFAULT_IMAGE_TOPIC,
+                         std::string twist_topic = DEFAULT_TWIST_TOPIC,
+                         int filter_size = FILTER_SIZE,
+                         double dropped_threshold = DROPPED_THRESHOLD,
+                         int max_points = MAX_POINTS,
+                         double deadband = DEADBAND_M,
+                         double robot_radius = ROBOT_RADIUS,
                          double rotation_constant = ROTATION_CONSTANT);
 
+    /**
+     * Destructor
+     */
     ~OpticalFlow();
 
+    /**
+     * ROS callback that handles the image processing
+     * @param image A sensor_msgs/ImageConstPtr message containing a compressed image from the RGB camera
+     */
     void imageCallback(const sensor_msgs::ImageConstPtr &image);
 
-    cv::Point3f feature_motion_estimate(std::vector<cv::Point2f> &prev_coords, std::vector<cv::Point2f> &curr_coords);
+    /**
+     * Function that estimates the average robot motion based on the motion of the different features in consecutive frames
+     * @param previous_coordinates The coordinates of the tracked features in the previous frame
+     * @param current_coordinates The coordinates of the tracked features in the current frame
+     * @return A point containing the averaged motion in the x,y,z axes (z is always zero for our case)
+     */
+    cv::Point3f feature_motion_estimate(std::vector<cv::Point2f> &previous_coordinates,
+                                        std::vector<cv::Point2f> &current_coordinates);
 
+    /**
+     * Set the filter size
+     * @param fs The new size of the filter (>0)
+     */
     void setFilterSize(int fs);
 
+    /**
+     * Set the number of tracked points
+     * @param pts The new number of tracked points (>0)
+     */
     void setMaxTrackedPoints(int pts);
 
+    /**
+     * Set the threshold to reset tracked features
+     * @param thresh The new threshold (in (0-1])
+     */
     void setDroppedThreshold(double thresh);
 
+    /**
+     * Set the deadband for ignoring small motions
+     * @param deadband The new deadband (in meters >= 0)
+     */
     void setDeadbandMeters(double deadband);
 
+    /**
+     * Set the robot radius
+     * @param radius The new robot radius (in meters >0)
+     */
     void setRobotRadius(double radius);
 
+    /**
+     * Set the rotation constant for motion calculation
+     * @param constant The new rotation constant
+     */
     void setRotationConstant(double constant);
 
+    /**
+     * Set the topic that OpticalFlow::image_subscriber_ uses to listen for images
+     * @param image_topic The new topic
+     */
     void setImageTopic(std::string image_topic);
 
+    /**
+     * Set the topic that OpticalFlow::twist_publisher_ uses to publish the estimated motion
+     * @param twist_topic The new topic
+     */
     void setTwistTopic(std::string twist_topic);
 
+    /**
+     * Return the filter size
+     * @return The filter size (OpticalFlow::filter_size_)
+     */
     int getFilterSize() const;
 
+    /**
+     * Return the number of tracked features
+     * @return The number of tracked features (OpticalFlow::max_points_)
+     */
     int getMaxTrackedPoints() const;
 
+    /**
+     * Return the dropped point threshold for generating a new feature set
+     * @return The threshold (OpticalFlow::dropped_threshold_)
+     */
     double getDroppedThreshold() const;
 
+    /**
+     * Return the deadband for motion to ignore in meters
+     * @return The deadband (OpticalFlow::deadband_)
+     */
     double getDeadBandMeters() const;
 
+    /**
+     * Return the robot radius (meters)
+     * @return The robot radius (OpticalFlow::robot_radius_)
+     */
     double getRobotRadius() const;
 
+    /**
+     * Return the rotation constant
+     * @return The rotation constant (OpticalFlow::rotation_constant_)
+     */
     double getRotationConstant() const;
 
+    /**
+     * Return the topic OpticalFlow::image_subscriber_ listens to for images
+     * @return The topic that OpticalFlow::image_subscriber_ listens to (OpticalFlow::image_topic_)
+     */
     std::string getImageTopic() const;
 
+    /**
+     * Return the topic OpticalFlow::twist_publisher_ publishes the estimated motion to
+     * @return The topic that OpticalFlow::twist_publisher_ publishes to (OpticalFlow::twist_topic_)
+     */
     std::string getTwistTopic() const;
 
+    /**
+     * Outputs the basic parameters of the current object
+     */
     void print();
 
 protected:
 private:
+    ///Needed for ROS Interface
     ros::NodeHandle nh_;
+    ///Publishes the estimated motion
     ros::Publisher twist_publisher_;
+    ///Needed for ROS image processing
     image_transport::ImageTransport image_transport_;
+    ///Listens for the incoming images
     image_transport::Subscriber image_subscriber_;
+    ///The ROS topic used by OpticalFlow::image_subsciber_
     std::string image_topic_;
+    ///The ROS topic used by OpticalFlow::twist_publisher_
     std::string twist_topic_;
+    ///The title of the visualization window
     std::string windowName_ = "Tracked Features";
-
+    ///A vector that stores recent motion estimates
     std::vector<geometry_msgs::Twist> filter_;
-
+    ///A CameraParameters instance that stores the used camera attributes
     CameraParameters cameraParameters_;
 
-    //orthogonal matrix
-    cv::Matx33d W_;
-    //Homography matrix
+    ///The homography mapping matrx for points between frames described in Chapter 9.2 in Multiple View Geometry in Computer Vision (Hartley-Zisserman)
     cv::Matx33d H_;
-    //Fundamental matrix
+    ///The fundamental matrix F described in Chapter 9.2 in Multiple View Geometry in Computer Vision (Hartley-Zisserman)
     cv::Matx33d F_;
-    //Essential matrix
+    ///The essential E matrix described in Chapter 9.6 in Multiple View Geometry in Computer Vision (Hartley-Zisserman)
     cv::Matx33d E_;
+    ///The W matrix used in Eq. 9.13 in Multiple View Geometry in Computer Vision (Hartley-Zisserman)
+    cv::Matx33d W_;
 
-    cv::Mat curr_;
-    cv::Mat prev_;
+    ///cv::Mat representation of the current frame
+    cv::Mat current_image_;
+    ///cv::Mat representation of the previous frame
+    cv::Mat previous_image_;
+    ///The error in the flow calculation for each feature
     std::vector<float> flow_errors_;
+    ///1 if the corresponding feature has been found during the flow calculation, 0 otherwise
     std::vector<unsigned char> flow_status_;
+    ///Coordinates of tracked features on the current frame
     std::vector<cv::Point2f> curr_track_indices_;
+    ///Coordinates of tracked features on the previous frame
     std::vector<cv::Point2f> prev_track_indices_;
-
+    ///The total accumulated motion so far (in X, Y, Z coordinates). Initialized to zero
     cv::Point3f accumulated_motion_ = cv::Point3f(0.0, 0.0, 0.0);
-
+    ///Size of stored frames to average motion
     int filter_size_;
+    ///Number of features to track
     int max_points_;
-    double dropped_threshold_;
-    double deadband_;
-    double robot_radius_;
-    double rotation_constant_;
-    double current_timestamp_;
-    double previous_timestamp_;
+    ///Determines the position of the new frame in the frame vector
     int filter_counter_ = 0;
+    ///Percentage of features that are allowed to be dropped between frames before calculating a new set
+    double dropped_threshold_;
+    ///Motion less than this will be ignored to avoid accumulating errors (meters)
+    double deadband_;
+    ///The radius of the robotic platform (meters)
+    double robot_radius_;
+    ///Experimentally defined rotation constant for the motion calculation (see OpticalFlow::feature_motion_estimate)
+    double rotation_constant_;
+    ///Current frame timestamp in seconds
+    double current_timestamp_;
+    ///Previous frame timestamp in seconds
+    double previous_timestamp_;
 };
 
 #endif //VISUAL_ODOM_OPTICAL_FLOW_H
